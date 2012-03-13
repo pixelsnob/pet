bb5b655f1621ae83eeae143d693579ff43df8b24
Luis A. Echeverria <pixelsnob@gmail.com>
2012-03-13 16:40:30 -0700

    Added main layout

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,13 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+class IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
-        /* Initialize action controller here */
+    public function init() {
+        
     }
 
-    public function indexAction()
-    {
-        // action body
+    public function indexAction() {
+        
     }
-
-
 }
 
