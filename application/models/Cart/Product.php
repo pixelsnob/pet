cfed704815df829fe6e3234a4b29dc77124f86b1
Luis A Echeverria <pixelsnob@gmail.com>
2012-04-13 16:58:11 -0700

    Cleanup

--- application/models/Cart/Product.php
+++ application/models/Cart/Product.php
@@ -9,35 +9,9 @@ class Model_Cart_Product extends Pet_Model_Abstract {
      * @var array
      * 
      */
-    /*protected $_data = array(
-        'product_id' => null,
-        'product_type_id' => null,
-        'sku' => null,
-        'cost' => null,
-        'image' => null,
-        'active' => null,
-        
-        'name' => null,
-        'description' => null,
-
-        'zone_id' => null,
-        'term' => null,
-        'is_renewal' => null,
-
-        'download_format_id' => null,
-        'date' => null,
-        'path' => null,
-        'size' => null,
-        'thumb' => null,
-        'subscriber_only' => null,
-
-        'qty'  => 1
-    );*/
-
     protected $_data = array(
         'product' => null,
         'qty' => 1
-        //'gift' => 0
     );
 
     /**
