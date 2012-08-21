5f7268e7afcbcc0e44c0e001c57b1e8ffb5dc617
Luis A. Echeverria <snob@pixelsnob.com>
2012-08-20 22:47:57 -0700

    send_order_emails cli script tweaks

--- application/bin/send_order_email.php
+++ application/bin/send_order_email.php
@@ -1,23 +1,29 @@
 <?php
 /**
- * Sends unsent order emails
+ * Sends an order email
  * 
  */
 require_once 'zf_init.php';
 
-$application->getBootstrap()->bootstrap(
-    array('router', 'config', 'registryView', 'db', 'logger', 'autoload'));
+try {
+    $application->getBootstrap()->bootstrap(
+        array('router', 'config', 'registryView', 'db', 'autoload', 'mail'));
 
-// Get command line opts
-$opts = getopt('o:d');
-if (!isset($opts['o']) || !strlen($opts['o'])) {
-    echo "Please pass the order_id as option -o\n";
+    // Get command line opts
+    $opts = getopt('o:d');
+    if (!isset($opts['o']) || !strlen($opts['o'])) {
+        echo "Please pass the order_id as option -o\n";
+        exit(1);
+    }
+
+    $order_id = $opts['o'];
+    $debug = isset($opts['d']);
+    $orders_svc = new Service_Orders;
+    $orders_svc->sendOrderEmail($order_id, $debug);
+    echo "Mail sent!\n";
+} catch (Exception $e){ 
+    echo 'Mail not sent. Error: ' . $e->getMessage() . "\n";
     exit(1);
 }
-
-$order_id = $opts['o'];
-$debug = isset($opts['d']);
-$orders_svc = new Service_Orders;
-$orders_svc->sendOrderEmail($order_id, $debug);
 
 exit(0);
