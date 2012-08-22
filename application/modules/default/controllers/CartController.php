9b86418b225a922a3a1a0c5e11d47e8eeda93478
Luis A. Echeverria <pixelsnob@gmail.com>
2012-08-21 21:45:06 -0700

    Changed gift redeemed message

--- application/modules/default/controllers/CartController.php
+++ application/modules/default/controllers/CartController.php
@@ -97,7 +97,9 @@ class CartController extends Zend_Controller_Action {
         $token = $this->_request->getParam('token');
         if ($this->_cart_svc->redeemGift($token)) {
             $this->_messenger->setNamespace('checkout');
-            $this->_messenger->addMessage('Your gift card was redeemed');
+            $msg = 'Welcome! To begin your gift subscription, please fill ' .
+                   'out the form below.';
+            $this->_messenger->addMessage($msg);
             $this->_helper->Redirector->setGotoRoute(array(), 'checkout');
         }
         // Fallback will show a "your gift was not redeemed" message
