4e5bf05063bb726ce893e273c834a7ac3bd38cfe
Luis A. Echeverria <pixelsnob@gmail.com>
2012-12-12 12:33:12 -0800

    Product description changes sql

--- var/mysql/products_changes.sql
+++ var/mysql/products_changes.sql
@@ -1,54 +1,61 @@
+
+use pet_tom;
+
 alter table products add name varchar(100) after sku;
 alter table products add description text after name;
 alter table products add short_description varchar(150) after description;
 
 /* Courses */
 
-update products p, courses c
+/*update products p, courses c
 set p.name = c.name,
 p.description = c.description,
 p.short_description = c.name
-where p.id = c.product_id;
+where p.id = c.product_id;*/
 
 alter table courses drop column description;
 alter table courses drop column name;
 
 /* Digital subs */
-update products p, digital_subscriptions d
+
+/*update products p, digital_subscriptions d
 set p.name = d.name,
 p.description = d.description,
 p.short_description = d.name
-where p.id = d.product_id;
+where p.id = d.product_id;*/
 
 alter table digital_subscriptions drop column description;
 alter table digital_subscriptions drop column name;
 
 /* Physical prods */
-update products p, physical_products pp
+
+/*update products p, physical_products pp
 set p.name = pp.name,
 p.description = pp.description,
 p.short_description = pp.name
-where p.id = pp.product_id;
+where p.id = pp.product_id;*/
 
 alter table physical_products drop column description;
 alter table physical_products drop column name;
 
 /* Subscriptions */
-update products p, subscriptions s
+
+/*update products p, subscriptions s
 set p.name = s.name,
 p.description = s.description,
 p.short_description = s.name
-where p.id = s.product_id;
+where p.id = s.product_id;*/
 
 alter table subscriptions drop column description;
 alter table subscriptions drop column name;
 
 /* Downloads */
-update products p, downloads d
+
+/*update products p, downloads d
 set p.name = d.name,
 p.description = d.description,
 p.short_description = d.name
-where p.id = d.product_id;
+where p.id = d.product_id;*/
 
 alter table downloads drop column description;
 alter table downloads drop column name;
