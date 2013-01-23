d2d3728979f1276740bc19d9cbd30233e22ccca4
Luis A. Echeverria <snob@pixelsnob.com>
2013-01-23 11:06:17 -0800

    Added debug functionality to send_order_email.php script

--- application/bin/send_order_email.php
+++ application/bin/send_order_email.php
@@ -5,25 +5,20 @@
  */
 require_once 'zf_init.php';
 
-//try {
-    $application->getBootstrap()->bootstrap(
-        array('router', 'config', 'registryView', 'db', 'autoload', 'mail'));
+$application->getBootstrap()->bootstrap(
+    array('router', 'config', 'registryView', 'db', 'autoload', 'mail'));
 
-    // Get command line opts
-    $opts = getopt('o:d');
-    if (!isset($opts['o']) || !strlen($opts['o'])) {
-        echo "Please pass the order_id as option -o\n";
-        exit(1);
-    }
-
-    $order_id = $opts['o'];
-    $debug = isset($opts['d']);
-    $orders_svc = new Service_Orders;
-    $orders_svc->sendOrderEmail($order_id, $debug);
-    echo "Mail sent!\n";
-/*} catch (Exception $e){ 
-    echo 'Mail not sent. Error: ' . $e->getMessage() . "\n";
+// Get command line opts
+$opts = getopt('o:d');
+if (!isset($opts['o']) || !strlen($opts['o'])) {
+    echo "Please pass the order_id as option -o\n";
     exit(1);
-}*/
+}
+
+$order_id = $opts['o'];
+$debug = isset($opts['d']);
+$orders_svc = new Service_Orders;
+$orders_svc->sendOrderEmail($order_id, $debug);
+echo "Mail sent!\n";
 
 exit(0);
